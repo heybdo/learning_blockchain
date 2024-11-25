ed3f1dfaa2994d7e304b3d6fb950a7c87b962c24
Bruno De Oliveira <brunodeoliveira@MacBook-Air-2.local>
2024-11-25 18:34:07 -0300

    add interface

--- bank/ibank.sol
+++ bank/ibank.sol
@@ -7,7 +7,7 @@ interface ibank {
     function withdraw(uint amount, address token) external;
     function deposit(uint amount, address token) external;
     function approvePayment(uint amount, address token) external;
-    function exchangeRate(address tokenA, address tokenB) external view;
+    function exchangeRate(address oracle) external view returns (int256);
     function exchange(address tokenA, address tokenB, uint amount) external;
     function getBalance (address account, address token) external view returns (uint);
 }
